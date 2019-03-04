038beefe0750053d4bcc38fa9c58b452b0156c92
stupendous-trainer-5541 <peter.c.viss@biola.edu>
2019-03-04 18:28:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number_to_test)
   return false if number_to_test < 2
-  (2..Math.sqrt(number_to_test)).each do |num|
+  (2..(number_to_test - 1)).each do |num|
     return false if number_to_test % num == 0
   end
   true
-end 
+end
